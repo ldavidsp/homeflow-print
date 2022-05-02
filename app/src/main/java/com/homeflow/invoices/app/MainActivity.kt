0730639918d2335545a33d490ac479ecf1b68283
Luis Solorzano <luisnic20@gmail.com>
2022-05-02 17:19:23 -0600

    Reformat code

--- app/src/main/java/com/homeflow/invoices/app/MainActivity.kt
+++ app/src/main/java/com/homeflow/invoices/app/MainActivity.kt
@@ -11,71 +11,69 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.homeflow.invoices.app.databinding.ActivityMainBinding
+import com.homeflow.printer.HomeflowPrint
 import com.homeflow.printer.core.Printable
 import com.homeflow.printer.core.TextPrintable
 
 class MainActivity : AppCompatActivity() {
 
-	private lateinit var appBarConfiguration: AppBarConfiguration
-	private lateinit var binding: ActivityMainBinding
+  private lateinit var appBarConfiguration: AppBarConfiguration
+  private lateinit var binding: ActivityMainBinding
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-		binding = ActivityMainBinding.inflate(layoutInflater)
-		setContentView(binding.root)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-		setSupportActionBar(binding.toolbar)
+    setSupportActionBar(binding.toolbar)
 
-		val navController = findNavController(R.id.nav_host_fragment_content_main)
-		appBarConfiguration = AppBarConfiguration(navController.graph)
-		setupActionBarWithNavController(navController, appBarConfiguration)
+    val navController = findNavController(R.id.nav_host_fragment_content_main)
+    appBarConfiguration = AppBarConfiguration(navController.graph)
+    setupActionBarWithNavController(navController, appBarConfiguration)
 
-		binding.fab.setOnClickListener { view ->
-			Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-				.setAction("Action", null).show()
+    binding.fab.setOnClickListener { view ->
+      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        .setAction("Action", null).show()
 
-			val print  = ArrayList<Printable>()
+      val print = ArrayList<Printable>()
 
-			print.add(TextPrintable.Builder().setLeft("TERMINAL:").setRight("342222").setNewLine())
-			print.add(TextPrintable.Builder().setLeft("NUM. TARJETA:").setRight("535353").setNewLine())
-			print.add(TextPrintable.Builder().setLine("----------------------------------------").setNewLine())
-			print.add(TextPrintable.Builder().setLeft("VENTA FACTURA Nro:").setRight("7373").setNewLine())
-			print.add(TextPrintable.Builder().setLeft("Fecha: 12/03/2000").setRight("Hora: $23:49").setNewLine())
-			print.add(TextPrintable.Builder().setLeft("Ref: 93939393").setRight("Autorizacion: $8383").setNewLine())
-			print.add(TextPrintable.Builder().setLeft("FORMA DE PAGO:").setRight("QPOS").setNewLine())
-			print.add(TextPrintable.Builder().setLeft("TOTAL:").setRight("7373").setNewLine())
+      print.add(TextPrintable.Builder().setLeft("TERMINAL:").setRight("342222").setNewLine())
+      print.add(TextPrintable.Builder().setLeft("NUM. TARJETA:").setRight("535353").setNewLine())
+      print.add(TextPrintable.Builder().setLine("----------------------------------------").setNewLine())
+      print.add(TextPrintable.Builder().setLeft("VENTA FACTURA Nro:").setRight("7373").setNewLine())
+      print.add(TextPrintable.Builder().setLeft("Fecha: 12/03/2000").setRight("Hora: $23:49").setNewLine())
+      print.add(TextPrintable.Builder().setLeft("Ref: 93939393").setRight("Autorizacion: $8383").setNewLine())
+      print.add(TextPrintable.Builder().setLeft("FORMA DE PAGO:").setRight("QPOS").setNewLine())
+      print.add(TextPrintable.Builder().setLeft("TOTAL:").setRight("7373").setNewLine())
 
-			print.add(TextPrintable.Builder().setLine("----------------------------------------").setNewLine())
-			print.add(TextPrintable.Builder().setCenter("No  requiere firma").setNewLine())
+      print.add(TextPrintable.Builder().setLine("----------------------------------------").setNewLine())
+      print.add(TextPrintable.Builder().setCenter("No  requiere firma").setNewLine())
 
-			val printer = Printer()
-			print.map {
-				Log.e("DATA: ", it.getPrintables())
-			}
-		}
+      Log.e("DATA: ", HomeflowPrint.previewLog(print))
+    }
 
-	}
+  }
 
-	override fun onCreateOptionsMenu(menu: Menu): Boolean {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		menuInflater.inflate(R.menu.menu_main, menu)
-		return true
-	}
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return true
+  }
 
-	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		return when (item.itemId) {
-			R.id.action_settings -> true
-			else -> super.onOptionsItemSelected(item)
-		}
-	}
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    return when (item.itemId) {
+      R.id.action_settings -> true
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
 
-	override fun onSupportNavigateUp(): Boolean {
-		val navController = findNavController(R.id.nav_host_fragment_content_main)
-		return navController.navigateUp(appBarConfiguration)
-			|| super.onSupportNavigateUp()
-	}
+  override fun onSupportNavigateUp(): Boolean {
+    val navController = findNavController(R.id.nav_host_fragment_content_main)
+    return navController.navigateUp(appBarConfiguration)
+      || super.onSupportNavigateUp()
+  }
 }
--- printer/src/main/java/com/homeflow/printer/HomeflowPrint.kt
+++ printer/src/main/java/com/homeflow/printer/HomeflowPrint.kt
@@ -2,9 +2,11 @@ package com.homeflow.printer
 
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
+import android.util.Log
 import com.dantsu.escposprinter.EscPosPrinter
 import com.dantsu.escposprinter.connection.bluetooth.BluetoothPrintersConnections
 import com.dantsu.escposprinter.connection.usb.UsbConnection
+import com.homeflow.printer.core.Printable
 
 /**
  * Homeflow print
@@ -16,22 +18,47 @@ object HomeflowPrint {
   /**
    * Print Bluetooth.
    *
-   * @param invoice String
+   * @param printables MutableList<Printable>
    */
-  fun bluetooth(invoice: String) {
+  fun bluetooth(printables: MutableList<Printable>) {
     val printer = EscPosPrinter(BluetoothPrintersConnections.selectFirstPaired(), 203, 48f, 32)
-    printer.printFormattedText(invoice)
+    printer.printFormattedText(rows(printables))
     printer.disconnectPrinter()
   }
 
   /**
    * Print USB.
    *
-   * @param invoice String
+   * @param printables MutableList<Printable>
    */
-  fun usb(invoice: String, usbManager: UsbManager, usbDevice: UsbDevice) {
+  fun usb(printables: MutableList<Printable>, usbManager: UsbManager, usbDevice: UsbDevice) {
     val printer = EscPosPrinter(UsbConnection(usbManager, usbDevice), 203, 48f, 32)
-    printer.printFormattedText(invoice)
+    printer.printFormattedText(rows(printables))
     printer.disconnectPrinter()
   }
+
+  /**
+   * Rows printable.
+   *
+   * @param printables MutableList<Printable>
+   * @return String
+   */
+  private fun rows(printables: MutableList<Printable>): String {
+    var printable = ""
+    printables.map { printable += it.getPrintables() }
+    return printable
+  }
+
+  /**
+   * Rows printable.
+   *
+   * @param printables MutableList<Printable>
+   * @return String
+   */
+  fun previewLog(printables: MutableList<Printable>): String {
+    var printable = "\n"
+    printables.map { printable += it.getPrintables() }
+    return printable
+  }
+
 }
